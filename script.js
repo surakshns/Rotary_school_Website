8b9e5def79c28c2758b9642816305107b4db0d91
surakshns <63912815+surakshns@users.noreply.github.com>
2020-09-25 01:19:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const com=document.querySelector("#com");
+const even=document.querySelector("#event");
+const gall=document.querySelector("#gallery");
+const don=document.querySelector("#donors");
+const cont=document.querySelector("#contact");
 (function (global) {
-  var homeHtml = "home.html"
+
+  var homeHtml = "home.html";
+  var comityHtml = "comity.html"
+  var eventHtml = "event.html"
+  var galleryHtml = "gallery.html"
+  var donorsHtml = "donors.html"
+  var contactHtml = "contact.html"
+
   var insertHtml = function (selector, html){
     var targetElem = document.querySelector(selector);
     targetElem.innerHTML = html;
@@ -19,16 +31,49 @@
     },
   false);
 });
-    var comityHtml = "comity.html"
-    var com = document.querySelector('.com');
-      com.addEventListener("click", function (event){
-        console.log("hey")
-      showLoading("#main");
-      $ajaxUtils.sendGetRequest(comityHtml,
-        function (responseText) {
-        document.querySelector("#main")
-        .innerHTML = responseText;
-      },
-    false);
-  });
+  com.addEventListener("click", function (event) {
+showLoading("#main");
+$ajaxUtils.sendGetRequest(comityHtml,
+  function (responseText) {
+  document.querySelector("#main")
+  .innerHTML = responseText;
+},
+false);
+});
+even.addEventListener("click", function (event) {
+showLoading("#main");
+$ajaxUtils.sendGetRequest(eventHtml,
+function (responseText) {
+document.querySelector("#main")
+.innerHTML = responseText;
+},
+false);
+});
+gall.addEventListener("click", function (event) {
+showLoading("#main");
+$ajaxUtils.sendGetRequest(galleryHtml,
+function (responseText) {
+document.querySelector("#main")
+.innerHTML = responseText;
+},
+false);
+});
+don.addEventListener("click", function (event) {
+showLoading("#main");
+$ajaxUtils.sendGetRequest(donorsHtml,
+function (responseText) {
+document.querySelector("#main")
+.innerHTML = responseText;
+},
+false);
+});
+cont.addEventListener("click", function (event) {
+showLoading("#main");
+$ajaxUtils.sendGetRequest(contactHtml,
+function (responseText) {
+document.querySelector("#main")
+.innerHTML = responseText;
+},
+false);
+});
 })(window);
