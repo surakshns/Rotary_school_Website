4e7789a470ca1969716084ec43ddd472f656d797
surakshns <63912815+surakshns@users.noreply.github.com>
2020-09-25 17:06:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const com=document.querySelector("#com");
-const even=document.querySelector("#event");
-const gall=document.querySelector("#gallery");
-const don=document.querySelector("#donors");
-const cont=document.querySelector("#contact");
+
 (function (global) {
 
   var homeHtml = "home.html";
   var comityHtml = "comity.html"
-  var eventHtml = "event.html"
-  var galleryHtml = "gallery.html"
-  var donorsHtml = "donors.html"
-  var contactHtml = "contact.html"
 
   var insertHtml = function (selector, html){
     var targetElem = document.querySelector(selector);
@@ -31,7 +23,26 @@ const cont=document.querySelector("#contact");
     },
   false);
 });
-  com.addEventListener("click", function (event) {
+})(window);
+  var comityHtml = "comity.html";
+  var eventHtml = "event.html";
+  var gallHtml = "gallery.html";
+  var donorHtml = "donor.html";
+  var contact = "contact.html";
+
+var insertHtml = function (selector, html){
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img id='icon' src='images/icon.webp'></div>";
+  insertHtml(selector, html);
+};
+
+function com(){
+console.log("hey");
 showLoading("#main");
 $ajaxUtils.sendGetRequest(comityHtml,
   function (responseText) {
@@ -39,41 +50,48 @@ $ajaxUtils.sendGetRequest(comityHtml,
   .innerHTML = responseText;
 },
 false);
-});
-even.addEventListener("click", function (event) {
+};
+
+function eve(){
+console.log("hey");
 showLoading("#main");
 $ajaxUtils.sendGetRequest(eventHtml,
-function (responseText) {
-document.querySelector("#main")
-.innerHTML = responseText;
+  function (responseText) {
+  document.querySelector("#main")
+  .innerHTML = responseText;
 },
 false);
-});
-gall.addEventListener("click", function (event) {
+};
+
+function gall(){
+console.log("hey");
 showLoading("#main");
-$ajaxUtils.sendGetRequest(galleryHtml,
-function (responseText) {
-document.querySelector("#main")
-.innerHTML = responseText;
+$ajaxUtils.sendGetRequest(gallHtml,
+  function (responseText) {
+  document.querySelector("#main")
+  .innerHTML = responseText;
 },
 false);
-});
-don.addEventListener("click", function (event) {
+};
+
+function donor(){
+console.log("hey");
 showLoading("#main");
-$ajaxUtils.sendGetRequest(donorsHtml,
-function (responseText) {
-document.querySelector("#main")
-.innerHTML = responseText;
+$ajaxUtils.sendGetRequest(donorHtml,
+  function (responseText) {
+  document.querySelector("#main")
+  .innerHTML = responseText;
 },
 false);
-});
-cont.addEventListener("click", function (event) {
+};
+
+function cont(){
+console.log("hey");
 showLoading("#main");
 $ajaxUtils.sendGetRequest(contactHtml,
-function (responseText) {
-document.querySelector("#main")
-.innerHTML = responseText;
+  function (responseText) {
+  document.querySelector("#main")
+  .innerHTML = responseText;
 },
 false);
-});
-})(window);
+};
