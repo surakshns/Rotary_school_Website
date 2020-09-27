3e704c4a0efbec883dd7d534c94b80d4c96a2803
surakshns <63912815+surakshns@users.noreply.github.com>
2020-09-27 14:30:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,66 +43,163 @@ var showLoading = function (selector) {
 
 function hom(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+var name = document.querySelector("#home").className;
+name += " active";
+document.querySelector("#home").className = name;
+
 showLoading("#main");
 $ajaxUtils.sendGetRequest(homHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
 };
 
 function com(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+var name = document.querySelector("#com").className;
+name += " active";
+document.querySelector("#com").className = name;
+
+
+console.log("hey");
 showLoading("#main");
 $ajaxUtils.sendGetRequest(comityHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
 };
 
 function eve(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+var name = document.querySelector("#eve").className;
+name += " active";
+document.querySelector("#eve").className = name;
+
 showLoading("#main");
 $ajaxUtils.sendGetRequest(eventHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
 };
 
 function gall(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+var name = document.querySelector("#gall").className;
+name += " active";
+document.querySelector("#gall").className = name;
+
 showLoading("#main");
 $ajaxUtils.sendGetRequest(gallHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
 };
 
 function donor(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+var name = document.querySelector("#donor").className;
+name += " active";
+document.querySelector("#donor").className = name;
+
 showLoading("#main");
 $ajaxUtils.sendGetRequest(donorHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
 };
 
 function cont(){
 console.log("hey");
+
+var classe = document.querySelector(".active").className;
+classe = classe.replace(new RegExp("active", "g"), "");
+document.querySelector(".active").className = classe;
+
+
+var name = document.querySelector("#cont").className;
+name += " active";
+document.querySelector("#cont").className = name;
+
 showLoading("#main");
 $ajaxUtils.sendGetRequest(contactHtml,
   function (responseText) {
   document.querySelector("#main")
   .innerHTML = responseText;
+
+  var classes = document.querySelector(".navbar-collapse").className;
+  classes = classes.replace(new RegExp("show", "g"), " ");
+  document.querySelector(".navbar-collapse").className = classes;
 },
 false);
+};
+
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+
+  console.log("hey");
+  var classes = document.querySelector(".active").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector(".active").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#com").className;
+  if (classes.indexOf("active") === -1) {
+    classes += " active";
+    document.querySelector("#com").className = classes;
+  }
 };
