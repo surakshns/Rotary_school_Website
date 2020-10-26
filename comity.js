57f679fddcb8d7c47f11fa4388ac0d0bfab12fa0
Suraksh N.S <63912815+surakshns@users.noreply.github.com>
2020-10-26 12:53:56 +0530

    Add files via upload

--- comity.js
+++ comity.js
@@ -13,6 +13,14 @@ var staff={
 
 }
 
+function comity(){
+  com();
+ incerting(management);
+ incerting(members);
+  console.log("async");
+}
+
+
 function incerting(a){
 var card = document.querySelector(".management");
 var mem = document.querySelector(".members");
@@ -37,9 +45,3 @@ else if (a==members) {
     place.innerHTML=a[index].place;
   });
 };
-function comity(){
-  com();
- incerting(management);
- incerting(members);
-  console.log("async");
-}
