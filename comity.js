2b894f728392bacdc3bfcefb7663a9fa27d178e8
Suraksh N.S <63912815+surakshns@users.noreply.github.com>
2020-10-26 17:26:59 +0530

    Add files via upload

--- comity.js
+++ comity.js
@@ -13,9 +13,9 @@ var staff={
 
 }
 
-function incerting(a){
-var card = document.querySelector(".management");
-var mem = document.querySelector(".members");
+async function incerting(a){
+await var card = document.querySelector(".management");
+await var mem = document.querySelector(".members");
 console.log(a);
 var block = "<div class='card'><img class='card-img-top image' src='' alt='Card image cap'><div class='card-body'><h6 class='card-title text-center place'></h6><h5 class='card-title text-center name'></h5></div><div>";
 console.log(block);
